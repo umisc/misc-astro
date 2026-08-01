c10009bc7d790ab5df51d89a43b02c6787d8189d
will <will.bradshaw50@gmail.com>
2026-08-01 21:57:10 +1000

    feat: seo improvements

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,19 +2,23 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import react from '@astrojs/react';
+import sitemap from '@astrojs/sitemap';
 import tailwindcss from '@tailwindcss/vite';
 
 import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
-  security: {
-    csp: false,
-  },
+  site: 'https://www.umisc.club',
   vite: {
     plugins: [tailwindcss()],
   },
-
-  integrations: [mdx(), react()],
-  adapter: vercel({ staticHeaders: true }),
+  integrations: [
+    mdx(),
+    react(),
+    sitemap({
+      filter: (page) => !page.endsWith('/design-system/'),
+    }),
+  ],
+  adapter: vercel(),
 });
